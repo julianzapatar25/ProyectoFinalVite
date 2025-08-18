e3ead251f40c76fcebb55d367ad6e6718d7e94e1
Julian Zapata Rojas <julianzapata.r25@gmail.com>
2025-08-18 13:24:18 -0500

    Guardado v13

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import  Header  from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Section from './components/Section/Section';
+import Contacto from './components/Contacto/Contacto';
 import './App.css';
 
 function App() {
@@ -10,6 +11,11 @@ function App() {
       <Header tittle="Bienvenidos a mi sitio web" show={true}/>
       <Section />
       <Footer />
+      <Contacto />
+      <p className="footer-text">© 2025 Mi Sitio Web. Todos los derechos reservados.</p>
+      <p className="footer-text">Desarrollado por Julian Zapata - Julián Zapata</p>
+       
+
     </>
 
 
