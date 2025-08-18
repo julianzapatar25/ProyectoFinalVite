ffe73c9272edc51c92c98b2f3a21822006765823
Julian Zapata Rojas <julianzapata.r25@gmail.com>
2025-08-17 23:43:42 -0500

    Guardado v7

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
+import React from "react";
+import "./Footer.css"; // Importamos el CSS
+import logo from "../../assets/logo.png"; // Importamos el logo
 
 const Footer = () => {
-    return (
-        <footer>
-            <p>Este es el Footer, gracias por visitar.
-            </p>
-        </footer>
-    )
-}
+  return (
+    <div className="footer-container">
+      <footer className="footer">
+        <ul className="footer-nav">
+          <li className="footer-item">
+            <a href="#" className="footer-link">Inicio</a>
+          </li>
+          <li className="footer-item">
+            <a href="#" className="footer-link">Nosotros</a>
+          </li>
+          <li className="footer-item">
+            <a href="#" className="footer-link">Servicios</a>
+          </li>
+          <li className="footer-item">
+            <a href="#" className="footer-link">FAQs</a>
+          </li>
+          <li className="footer-item">
+            <a href="#" className="footer-link">Contáctenos</a>
+          </li>
+        </ul>
+        <p className="footer-copy">
+          &copy; {new Date().getFullYear()} Company, Inc. Todos los derechos reservados.
+        </p>
+      </footer>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
+
