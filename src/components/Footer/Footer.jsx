a2c3b47846dea5ef46401c20c8954dc70ad2a936
Julian Zapata Rojas <julianzapata.r25@gmail.com>
2025-08-17 23:57:15 -0500

    Guardado v8

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,28 +1,25 @@
 import React from "react";
-import "./Footer.css"; // Importamos el CSS
-import logo from "../../assets/logo.png"; // Importamos el logo
+import "./Footer.css";
+import logo from "../../assets/logo.png"; // ajusta la ruta según tu estructura
 
 const Footer = () => {
   return (
     <div className="footer-container">
       <footer className="footer">
+        {/* Logo */}
+        <div className="footer-logo">
+          <img src={logo} alt="Logo Company" />
+        </div>
+
+        {/* Menú */}
         <ul className="footer-nav">
-          <li className="footer-item">
-            <a href="#" className="footer-link">Inicio</a>
-          </li>
-          <li className="footer-item">
-            <a href="#" className="footer-link">Nosotros</a>
-          </li>
-          <li className="footer-item">
-            <a href="#" className="footer-link">Servicios</a>
-          </li>
-          <li className="footer-item">
-            <a href="#" className="footer-link">FAQs</a>
-          </li>
-          <li className="footer-item">
-            <a href="#" className="footer-link">Contáctenos</a>
-          </li>
+          <li className="footer-item"><a href="#" className="footer-link">Inicio</a></li>
+          <li className="footer-item"><a href="#" className="footer-link">Nosotros</a></li>
+          <li className="footer-item"><a href="#" className="footer-link">Servicios</a></li>
+          <li className="footer-item"><a href="#" className="footer-link">FAQs</a></li>
+          <li className="footer-item"><a href="#" className="footer-link">Contáctenos</a></li>
         </ul>
+
         <p className="footer-copy">
           &copy; {new Date().getFullYear()} Company, Inc. Todos los derechos reservados.
         </p>
@@ -32,4 +29,5 @@ const Footer = () => {
 };
 
 export default Footer;
+
 
