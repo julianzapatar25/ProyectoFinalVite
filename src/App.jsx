21e375742cc19df3f6b0679f050368cd8eaa950e
Julian Zapata Rojas <julianzapata.r25@gmail.com>
2025-08-18 13:18:42 -0500

    Guardado v14

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,19 @@
-import Header from './components/Header/Header';
+import  Header  from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Section from './components/Section/Section';
-import Contacto from './components/Contacto/Contacto';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 
 function App() {
+
   return (
-    <Router>
+    <>
       <Header tittle="Bienvenidos a mi sitio web" show={true}/>
-      
-      {/* Menú de navegación simple */}
-      <nav className="main-nav">
-        <Link to="/">Inicio</Link>
-        <Link to="/contacto">Contacto</Link>
-      </nav>
-
-      <Routes>
-        <Route path="/" element={<Section />} />
-        <Route path="/contacto" element={<Contacto />} />
-      </Routes>
-
+      <Section />
       <Footer />
-    </Router>
+    </>
+
+
   )
 }
 
-export default App;
+export default App
